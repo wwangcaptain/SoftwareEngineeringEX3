87064284b16195e0b5c309e331b1fc97bb64360d
WangSheng <Wong092105@outlook.com>
2024-10-04 00:53:51 +0800

    使用另一个账号上传来模拟冲突

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, 2022155007 汪升" << std::endl;
+    std::cout << "Hello, 2022155007 汪升！！！" << std::endl;
+std::cout << "Hello, world！" << std::endl;
     return 0;
 }
