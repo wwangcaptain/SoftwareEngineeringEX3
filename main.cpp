9114f0c570a3cf16c4d20410fcee1f8b5f138710
Wang Sheng <143316086+wwangcaptain@users.noreply.github.com>
2024-10-04 01:02:07 +0800

    Add files via upload
    模拟冲突

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, 2022155007 汪升！！！" << std::endl;
-std::cout << "Hello, world！" << std::endl;
+    std::cout << "Hello, 2022155007" << std::endl;
     return 0;
 }
